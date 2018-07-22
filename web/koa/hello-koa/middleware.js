41f92f5556c2f88c2f08caf21150eaaf9ed56f1f
mayner <meiningzhu@163.com>
2018-07-22 23:39:58 +0800

    add url-koa

--- web/koa/hello-koa/middleware.js
+++ web/koa/hello-koa/middleware.js
@@ -1,5 +1,6 @@
 /* 
-koa把很多async函数组成一个处理链，每个async函数都可以做一些自己的事情，然后用await next()来调用下一个async函数。我们把每个async函数称为middleware，这些middleware可以组合起来，完成很多有用的功能。
+koa把很多async函数组成一个处理链，每个async函数都可以做一些自己的事情，然后用await next()来调用下一个async函数。
+我们把每个async函数称为middleware，这些middleware可以组合起来，完成很多有用的功能。
 
 例如，可以用以下3个middleware组成处理链，依次打印日志，记录处理时间，输出HTML：
 */
@@ -22,7 +23,8 @@ app.use(async (ctx, next) => {
 
 /* 
 middleware的顺序很重要，也就是调用app.use()的顺序决定了middleware的顺序。
-此外，如果一个middleware没有调用await next()，会怎么办？答案是后续的middleware将不再执行了。这种情况也很常见，例如，一个检测用户权限的middleware可以决定是否继续处理请求，还是直接返回403错误：
+此外，如果一个middleware没有调用await next()，会怎么办？答案是后续的middleware将不再执行了。
+这种情况也很常见，例如，一个检测用户权限的middleware可以决定是否继续处理请求，还是直接返回403错误：
 */
 
 app.use(async (ctx, next) => {
