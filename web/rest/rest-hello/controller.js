4df223f6084101d5a118d8ff12946d8e797b5e54
mayner <meiningzhu@163.com>
2018-08-03 17:30:57 +0800

    add rest-hello

---
+++ web/rest/rest-hello/controller.js
@@ -0,0 +1,50 @@
+// 先导入fs模块，然后用readdirSync列出文件
+const fs = require('fs');
+
+function addMapping(router, mapping) {
+    for (var url in mapping) {
+        if (url.startsWith('GET ')) {
+            // 如果url类似"GET xxx":
+            var path = url.substring(4);
+            router.get(path, mapping[url]);
+            console.log(`register URL mapping: GET ${path}`);
+        } else if (url.startsWith('POST ')) {
+            // 如果url类似"POST xxx":
+            var path = url.substring(5);
+            router.post(path, mapping[url]);
+            console.log(`register URL mapping: POST ${path}`);
+        } else if (url.startsWith('PUT ')) {
+            // 如果url类似"PUT xxx":
+            var path = url.substring(4);
+            router.put(path, mapping[url]);
+            console.log(`register URL mapping: PUT ${path}`);
+        } else if (url.startsWith('DELETE ')) {
+            // 如果url类似"DELETE xxx":
+            var path = url.substring(7);
+            router.del(path, mapping[url]);
+            console.log(`register URL mapping: DELETE ${path}`);
+        } else {
+            // 无效的URL:
+            console.log(`invalid URL: ${url}`);
+        }
+    }
+}
+
+function addControllers(router, dir) {
+    fs.readdirSync(__dirname + '/' + dir).filter((f) => {
+        return f.endsWith('.js');
+    }).forEach((f) => {
+        console.log(`Process controller: ${f}...`);
+        // 导入js文件:
+        let mapping = require(__dirname + '/' + dir + '/' + f);
+        addMapping(router, mapping);
+    });
+}
+
+module.exports = function (dir) {
+    let 
+        controllers_dir = dir || 'controllers',  // 如果不传参数，扫描目录默认为'controllers'
+        router = require('koa-router')();
+    addControllers(router, controllers_dir);
+    return router.routes();
+}
